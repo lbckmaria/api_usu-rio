44bdba095496db3a01301ee6b579c6b4a492e46b
Maria Lembeck <maria.lembeck@alunos.sc.senac.br>
2025-04-01 16:20:05 -0300

    atualizaçãp

--- index.php
+++ index.php
@@ -8,52 +8,40 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$errorMiddleware = $app->addErrorMiddleware(true,true,true);
-$errorMiddleware->setErrorHandler(HttpNotFoundException::class,function (
-    Request   $request,
-    Throwable $exception, 
-    bool $displayErrorDetails,
-    bool $logErrors,
-    bool $logErrorDetails                                     
-) use ($app) {
-    $response = $app->getResponseFactory()->createResponse();
-    $response ->getBody()->write('{"error": "Recurso não foi encontrado"}');
-    return $response->withHeader('Content-Type', 'application/json')->withStatus(404);
-});
-
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
-$app->get('/tarefas', function (Request $request, Response $response, array $args) {
-    $tarefas = [
-        ["id" => 1, "titulo" => "Ler a Documentação do Slim", "concluido" => false],
-        ["id" => 3, "titulo" => "Ler a Documentação do composer", "concluido" => true],
-        ["id" => 4, "titulo" => "Faer um lanche", "concluido" => true],
-        ["id" => 5, "titulo" => "Limpar o quarto", "concluido" => false],
+$app->get('/usuarios', function (Request $request, Response $response, array $args) {
+    $usuarios = [
+        ["id"=>1, "login"=> "elisa.fonseca", "nome"=> "Elisa Fonseca"],
+        ["id"=>2, "login"=> "isabella.daberkow", "nome"=> "Isabella Daberkow"],
+        ["id"=>3, "login"=> "karina.meier", "nome"=> "Karina Meier"],
+        ["id"=>4, "login"=> "amabile.luz", "nome"=> "Amábile Luz"],
+        ["id"=>5, "login"=> "maria.lembeck", "nome"=> "Maria Lembeck"],
     ];
-    $response->getBody()->write(json_enode($tarefas));
+    $response->getBody()->write(json_encode($usuarios));
     return $response->withHeader('Content-Type', 'application/json');
 });
-
-$app->post('/tarefas', function(Request $request, Response $response, array $args){
-
+ 
+$app->post('/usuarios', function(Request $request, Response $response, array $args){
+    $parametros = (array) $request->getParsedBody();
+    if (!array_key_exists('login', $parametros) || empty($parametros['login'])){
+        $response->getBody()->write(json_encode([
+            "mensagem" => "Login é obrigaório"
+        ]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+    }
+    if (!array_key_exists('senha', $parametros) || empty($parametros['senha'])){
+        $response->getBody()->write(json_encode([
+            "mensagem" => "Senha é obrigaório"
+        ]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+    }
     return $response->withStatus(201);
-
 });
-  
-app->delete('/tarefas', function(Request $request, Response $response, array $args){
-
-    return $response->withStatus(204);
-
-});
-
-app->put('/tarefas', function(Request $request, Response $response, array $args){
-
+$app->delete('/usuarios/{id}', function(Request $requets, Response $response, array $args) {
     return $response->withStatus(201);
-
 });
+$app->put('/usuarios/{id}', function(Request $requets, Response $response, array $args) {
+    return $response->withStatus(201);
+});
+ 
 
 $app->run();
